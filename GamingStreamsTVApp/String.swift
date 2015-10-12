c0f6421491fcb068af3af43d2976474eac30dbbb
Brendan Kirchner <brendankirchner@me.com>
2015-10-12 08:51:15 -0400

    refactored the hex string extension into a UIColor extension changed around some optionals

--- GamingStreamsTVApp/String.swift
+++ GamingStreamsTVApp/String.swift
@@ -39,46 +39,6 @@ extension String {
 
 extension String {
     func toUIColorFromHex() -> UIColor? {
-        
-        var red:   CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue:  CGFloat = 0.0
-        var alpha: CGFloat = 1.0
-        
-        if self.characters.count == 7 && self.hasPrefix("#") {
-            let index   = self.startIndex.advancedBy(1)
-            let hex     = self.substringFromIndex(index)
-            let scanner = NSScanner(string: hex)
-            var hexValue: CUnsignedLongLong = 0
-            if scanner.scanHexLongLong(&hexValue) {
-                switch (hex.characters.count) {
-                case 3:
-                    red   = CGFloat((hexValue & 0xF00) >> 8)       / 15.0
-                    green = CGFloat((hexValue & 0x0F0) >> 4)       / 15.0
-                    blue  = CGFloat(hexValue & 0x00F)              / 15.0
-                case 4:
-                    red   = CGFloat((hexValue & 0xF000) >> 12)     / 15.0
-                    green = CGFloat((hexValue & 0x0F00) >> 8)      / 15.0
-                    blue  = CGFloat((hexValue & 0x00F0) >> 4)      / 15.0
-                    alpha = CGFloat(hexValue & 0x000F)             / 15.0
-                case 6:
-                    red   = CGFloat((hexValue & 0xFF0000) >> 16)   / 255.0
-                    green = CGFloat((hexValue & 0x00FF00) >> 8)    / 255.0
-                    blue  = CGFloat(hexValue & 0x0000FF)           / 255.0
-                case 8:
-                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-                default:
-                    NSLog("Invalid RGB Hex string, number of characters after '#' should be either 3, 4, 6 or 8")
-                    return nil
-                }
-            } else {
-                return nil
-            }
-        }
-        
-        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+        return UIColor(hexString: self)
     }
 }
